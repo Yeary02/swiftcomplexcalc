db048ed98cb8ec3787ae83555f7f62510e538503
Yeary02 <yyuan02@uw.edu>
2022-04-11 22:53:50 -0700

    part 2

--- ComplexCalc.playground/Contents.swift
+++ ComplexCalc.playground/Contents.swift
@@ -38,12 +38,31 @@ class Calculator {
     func mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int) -> Int {
         return op(lhs, rhs)
     }
+    
+    func count(_ args: [Int]) -> Int {
+        return args.count
+    }
+    
+    func avg(_ args: [Int]) -> Int {
+        if args.count == 1 {
+            return args[0]
+        }
+        let sum = add(args)
+        return sum / args.count
+    }
+    
+    func mathOp(args: [Int], beg: Int, op: (Int, Int) -> Int ) -> Int {
+        return op(add(args), beg)
+    }
+    
+//    func add(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+//        return (lhs + rhs)
+//    }
 }
 
 let calc = Calculator()  // Don't change this declaration name; it's used in all the tests below
 
 // ====> Add your own tests here if you wish <====
-
 
 // ====> Do not modify code in this section <====
 calc.add(lhs: 2, rhs: 2) == 4
@@ -52,24 +71,24 @@ calc.multiply(lhs: 2, rhs: 2) == 4
 calc.divide(lhs: 2, rhs: 2) == 1
 
 calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rhs) + (lhs * rhs) }) == 35
-//    // This style is one way of writing an anonymous function
+    // This style is one way of writing an anonymous function
 calc.mathOp(lhs: 10, rhs: -5, op: { ($0 + $1) + ($0 - $1) }) == 20
-//    // This is the second, more terse, style; either works
-//
+    // This is the second, more terse, style; either works
+
 calc.add([1, 2, 3, 4, 5]) == 15
 calc.multiply([1, 2, 3, 4, 5]) == 120
-//calc.count([1, 2, 3, 4, 5, 6, 7, 8]) == 8
-//calc.count([]) == 0
-//calc.avg([2, 2, 2, 2, 2, 2]) == 2
-//calc.avg([1, 2, 3, 4, 5]) == 3
-//calc.avg([1]) == 1
+calc.count([1, 2, 3, 4, 5, 6, 7, 8]) == 8
+calc.count([]) == 0
+calc.avg([2, 2, 2, 2, 2, 2]) == 2
+calc.avg([1, 2, 3, 4, 5]) == 3
+calc.avg([1]) == 1
 
-//calc.mathOp(args: [1, 2, 3], beg: 0, op: { $0 + $1 }) == 6
-//    // this is (((0 op 1) op 2) op 3)
-//calc.mathOp(args: [1, 2, 3, 4, 5], beg: 0, op: { $0 + $1 }) == 15
-//    // this is (((((0 op 1) op 2) op 3) op 4) op 5)
-//calc.mathOp(args: [1, 1, 1, 1, 1], beg: 1, op: { $0 * $1 }) == 1
-//    // this is (((((1 op 1) op 1) op 1) op 1) op 1)
+calc.mathOp(args: [1, 2, 3], beg: 0, op: { $0 + $1 }) == 6
+    // this is (((0 op 1) op 2) op 3)
+calc.mathOp(args: [1, 2, 3, 4, 5], beg: 0, op: { $0 + $1 }) == 15
+    // this is (((((0 op 1) op 2) op 3) op 4) op 5)
+calc.mathOp(args: [1, 1, 1, 1, 1], beg: 1, op: { $0 * $1 }) == 1
+    // this is (((((1 op 1) op 1) op 1) op 1) op 1)
 
 //let p1 = (5, 5)
 //let p2 = (12, -27)
